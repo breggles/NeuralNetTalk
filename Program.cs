3f46e31fc8d32df57dce27716b397eff443a87d0
breggles <tilman.bregler@red-gate.com>
2024-02-20 18:07:07 +0000

    faffing

--- Program.cs
+++ Program.cs
@@ -2,24 +2,20 @@
 using static System.Math;
 
 var w = 0.5;
-var b = 0.5;
 
-int[] xs = [1];
+int x = 1;
 int[] ys = [2];
 
 for (int i = 0; i < 130; i++)
 {
-    foreach (var x in xs)
-    {
-        WriteLine($"w = {w}");
-        WriteLine($"x = {x}");
-        var y = w * x;
-        WriteLine($"y = w * x = {w} * {x} = {y}");
-        var loss = Pow(y - ys[0], 2);
-        WriteLine($"loss = (y - expected)^2 = {loss}");
-        var grad = w;
-        WriteLine($"grad = dy/dx = {grad}");
-        w = w + 0.01 * grad;
-        WriteLine($"new w = {w}");
-    }
+    WriteLine($"w = {w}");
+    WriteLine($"x = {x}");
+    var y = w * x;
+    WriteLine($"y = w * x = {w} * {x} = {y}");
+    var loss = Pow(y - ys[0], 2);
+    WriteLine($"loss = (y - expected)^2 = {loss}");
+    var grad = x;
+    WriteLine($"grad = dy/dw = {grad}");
+    w = w + 0.01 * grad;
+    WriteLine($"new w = {w}");
 }
